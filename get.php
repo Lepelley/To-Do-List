ef26baa7f84f11e336393e1a0ea542a06cd50537
Vincent Lepelley <v.lepelley@gmail.com>
2019-10-01 22:22:19 +0200

    Update get.php

--- get.php
+++ get.php
@@ -1,2 +1,10 @@
 <?php
+    require_once 'config.php';
+
+    //  header('Content-Type: application/json');
     
+    echo json_encode(array('text' => 'omrele'));
+
+    $db = dbConnect();
+    $query = $db->query('SELECT id, content, status FROM todo_list');
+    echo json_encode($query->fetch());
