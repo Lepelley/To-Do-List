980fc334570848ff17a3ce94957dd1e9233a9cb9
Vincent Lepelley <v.lepelley@gmail.com>
2019-10-02 00:19:03 +0200

    Update post.php

--- post.php
+++ post.php
@@ -8,24 +8,24 @@
     // Converts it into a PHP object
     $data = json_decode($json);
 
-    if (isset($data) && $data['content'] != '') {
-        if ($data['id'] > 0) { // Update task
+    if ($data->content != '') {
+        if ($data->id > 0) { // Update task
             $query = $db->prepare('UPDATE todo_list SET content = :content, status = :status WHERE id = :id');
-            $query->bindParam(':content', htmlspecialchars($data['content']));
-            $query->bindParam(':status', $data['status'], PDO::PARAM_INT);
-            $query->bindParam(':id', $data['id'], PDO::PARAM_INT);
+            $query->bindParam(':content', htmlspecialchars($data->content));
+            $query->bindParam(':status', $data->status, PDO::PARAM_INT);
+            $query->bindParam(':id', $data->id, PDO::PARAM_INT);
             $query->execute();
         }
         else { // Add task
             $query = $db->prepare('INSERT INTO todo_list (content, status) VALUES (:content, :status)');
-            $query->bindParam(':content', htmlspecialchars($data['content']));
+            $query->bindParam(':content', htmlspecialchars($data->content));
             $query->bindParam(':status', 1);
             $query->execute();
         }
     }
-    elseif (isset($data) && $data['status'] == 0 && $data['id'] > 0) { // Delete task
+    elseif ($data->status == 0 && $data->id > 0) { // Delete task
         $query = $db->prepare('DELETE FROM todo_list WHERE id = :id');
-        $query->bindParam(':id', $data['id'], PDO::PARAM_INT);
+        $query->bindParam(':id', $data->id, PDO::PARAM_INT);
         $query->execute();
     }
     else {
