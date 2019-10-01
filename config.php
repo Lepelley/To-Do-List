e31abd3873f9a926c0fe9a1690cf1e2f9ddc9786
Vincent Lepelley <v.lepelley@gmail.com>
2019-10-01 22:31:33 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,12 +1,13 @@
 <?php
-    $dns = 'mysql';
-    $host = 'localhost';
-    $db = 'portfolio';
-    $user = 'root';
-    $password = '';
 
     function dbConnect()
     {
+        $dns = 'mysql';
+        $host = 'localhost';
+        $db = 'portfolio';
+        $user = 'root';
+        $password = '';
+
         $db = new \PDO($dns . ':host=' . $host . ';dbname=' . $db . ';charset=utf8', $user, $password);
         $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
         $db->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
