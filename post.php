251b7a9e472fb2a8b2a2b472f5333543ab35cd27
Vincent Lepelley <v.lepelley@gmail.com>
2019-10-02 02:05:45 +0200

    Update post.php

--- post.php
+++ post.php
@@ -14,22 +14,21 @@
         $query->execute();
         http_response_code(200); // OK
     }
-    elseif ($data->content != '') {
-        if ($data->id > 0) { // Update task
-            $query = $db->prepare('UPDATE todo_list SET content = :content, status = :status WHERE id = :id');
-            $query->bindParam(':content', htmlspecialchars($data->content));
-            $query->bindParam(':status', $data->status, PDO::PARAM_INT);
-            $query->bindParam(':id', $data->id, PDO::PARAM_INT);
-            $query->execute();
-            http_response_code(200); // OK
-        }
-        else { // Add task
+    elseif ($data->content != '' && $data->id > 0) { // Update task
+        $query = $db->prepare('UPDATE todo_list SET content = :content, status = :status WHERE id = :id');
+        $query->bindParam(':content', htmlspecialchars($data->content));
+        $query->bindParam(':status', $data->status, PDO::PARAM_INT);
+        $query->bindParam(':id', $data->id, PDO::PARAM_INT);
+        $query->execute();
+        http_response_code(200); // OK
+    }
+    elseif ($data->content != '' && !isset($data->id)) { // Add task
             $query = $db->prepare('INSERT INTO todo_list (content, status) VALUES (:content, :status)');
             $query->bindParam(':content', htmlspecialchars($data->content));
             $query->bindParam(':status', $data->status);
             $query->execute();
+            echo $db->lastInsertId();
             http_response_code(201); // Create
-            //echo $db->lastInsertId();
         }
     }
     else {
